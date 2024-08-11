c0dd229409f9ceb8b57e9e689e25e9c6804e9d95
Arnav Aggarwal <arnavaggarwal2012@gmail.com>
2024-08-12 01:54:42 +0530

    auth bg color change

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -14,7 +14,7 @@ const handleGoogleLogin = () => {
 const Auth = () => {
   return (
     <div className="h-screen flex flex-col">
-      <header className="bg-background top-0 left-0 w-full border-b">
+      <header className="bg-black top-0 left-0 w-full border-b">
         <div className="container flex h-16 items-center justify-center px-4 md:px-6">
           <img src={logo} height={150} width={150} />
         </div>
